2fb7b12a862bf35821cc44a164f174384b6dc60f
Javier Linares <jlinares-2020162@kinal.edu.gt>
2022-05-29 18:37:37 -0600

    Se agregó la funcionalidad del componente -login-.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashboardCompaniesComponent } from './components/compannies/dashboard-companies/dashboard-companies.component';
 import { LoginComponent } from './components/login/login.component';
+
 
 @NgModule({
   declarations: [
@@ -14,7 +17,9 @@ import { LoginComponent } from './components/login/login.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { CargarScriptsService } from 'src/app/cargar-scripts.service';
+import { CompanyModel } from 'src/app/models/company.model';
+import { CompanyRestService } from 'src/app/services/companyRest/company-rest.service';
+import { Router } from '@angular/router';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-login',
@@ -7,16 +10,42 @@ import { CargarScriptsService } from 'src/app/cargar-scripts.service';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  company:CompanyModel;
 
-  constructor
-  (
-    private _CargarScripts:CargarScriptsService,
-  ) 
-  {
-    _CargarScripts.Carga(["login"])
+
+  constructor(
+    private companyRest: CompanyRestService,
+    private router: Router
+  ) { 
+    this.company = new CompanyModel('', '', '', '', '', '', '', '');
   }
 
   ngOnInit(): void {
+    
+  }
+
+  login()
+  {
+    this.companyRest.login(this.company).subscribe({
+      next: (res:any)=>{
+        localStorage.setItem('identity', JSON.stringify(res.already));
+        localStorage.setItem('token', res.token);
+        
+        Swal.fire({
+          icon:'success',
+          title: res.message,
+          html:'Welcome <b>'+ res.already.username+'</b>',
+          confirmButtonColor: '#28B463'
+        })
+      },
+      error: (err: any) => {
+        Swal.fire({
+          icon: 'error',
+          title: err.error.message || err.error,
+          confirmButtonColor: '#E74C3C'
+        });
+      },
+    })
   }
 
 }
